e70a700777f48823015801da9e6551ba0085e609
ProgrammerFRiTZeR <159638249+ProgrammerFRiTZeR@users.noreply.github.com>
2025-04-28 13:42:57 +0800

    Add files via upload

---
+++ tasksheet_1_1_2.java
@@ -0,0 +1,18 @@
+public class tasksheet_1_1_2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        a += b;
+        System.out.println("Addition using compound assignment: " + a);
+
+        a -= b;
+        System.out.println("Subtraction using compound assignment: " + a);
+
+        a *= b;
+        System.out.println("Multiplication using compound assignment: " + a);
+
+        a /= b;
+        System.out.println("Division using compound assignment: " + a);
+    }
+}          
